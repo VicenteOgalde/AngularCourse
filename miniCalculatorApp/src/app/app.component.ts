5ee116ccc6c8a3e95c1e9aa3ab67a70edf6cd559
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-14 19:23:49 -0300

    half of the calculator done

--- miniCalculatorApp/src/app/app.component.ts
+++ miniCalculatorApp/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculatorApp';
+  title = 'Mini Calculator App';
+  number1:number=0;
+  number2:number=0;
+  result:number=0;
+
+  public sum():void{
+    this.result=this.number1+this.number2;
+  }
+
+
+
 }
