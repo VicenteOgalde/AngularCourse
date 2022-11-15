194679ce98c06af9089ad6afdc7dda6efd5d1dfe
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-15 14:13:24 -0300

    added else and ngFor

--- workDirectiveApp/src/app/app.component.ts
+++ workDirectiveApp/src/app/app.component.ts
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  users:object[]=[];
   title = 'User Register';
   message:string='';
   name:string='';
   surname:string='';
 
+
+
   public userRegister():void{
+    this.users.push({Name:this.name},{Surname:this.surname});
     this.message='successful register';
   }
 }
