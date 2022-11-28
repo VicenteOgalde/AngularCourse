497538137a9c427ab98c8b8e3502aa3dcc938370
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-27 21:13:55 -0300

    routing

--- formApp/src/app/projects-component/projects-component.component.ts
+++ formApp/src/app/projects-component/projects-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeServiceService } from '../employee-service.service';
+import { Employee } from '../employee.model';
+import { EmployeeArrayService } from '../employeeArray.service';
 
 @Component({
   selector: 'app-projects-component',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router,private serviceArray:EmployeeArrayService ) { 
+    this.employees=serviceArray.employees;
+  }
 
   ngOnInit(): void {
+
   }
+  backToHome(){
+    this.router.navigate(['']);
+
+  }
+  name:string='';
+  surname:string='';
+  position:string='';
+  salary:number=0;
+
+  employees:Employee[]=[];
+  public registerEmployee():void{
+    // this.meService.showOkMessage(`Employee ${this.name} with position ${this.position} added` )
+     this.serviceArray.AddEmployeeService(this.name,this.surname,this.position,Number(this.salary));
+     this.router.navigate(['']);
+ 
+   }
 
 }
